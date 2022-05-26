f310163b8765307dab96f388814700427032a172
aslaugsol <70697649+aslaugsol@users.noreply.github.com>
2022-05-26 22:10:23 +0000

    laga í base.html

--- django_projects/Event_driven/static/js/events.js
+++ django_projects/Event_driven/static/js/events.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         var SearchText = $('#search-box').val();
         $.ajax({
             url:'/events?search_filter=' + SearchText,
-            type: 'GET'
+            type: 'GET',
             success: function(resp){
                 var newHtml = resp.data.map(d => {
                     return '<div class="well event"><a href="/events/${d.id}"><img class="event-img" src="${d.firstImage}"/><h4>${d.name}</h4><p>${d.categoryy}</p></a></div>'
