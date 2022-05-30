674f2e624db93202058e50dbcb5c717ec31e1814
aslaugsol <70697649+aslaugsol@users.noreply.github.com>
2022-05-30 19:01:38 +0000

    event booking

--- django_projects/Event_driven/EventDriven/views.py
+++ django_projects/Event_driven/EventDriven/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from EventDriven.forms.event_form import EventCreateForm, EventUpdateForm
-from EventDriven.models import Event
+from EventDriven.models import Event, Cart
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
-#def start(request):
+# def start(request):
 #    return render(request, 'events/indexx.html')
 
 def index(request):
@@ -15,16 +16,18 @@ def index(request):
             'id': x.id,
             'name': x.name,
             'category': x.category,
-            #'firstImage': c.eventimage_set.first().image #Þarf að bæta við image model-i
+            # 'firstImage': c.eventimage_set.first().image #Þarf að bæta við image model-i
         } for x in Event.objects.filter(name__icontains=search_filter)]
-        events = list(Event.objects.filter(name__icontains=search_filter).values() )
+        events = list(Event.objects.filter(name__icontains=search_filter).values())
         return JsonResponse({'data': events})
     context = {'events': Event.objects.all().order_by('name')}
-    return render(request, 'events/indexx.html', context )
+    return render(request, 'events/indexx.html', context)
+
 
 def get_event_by_id(request, id):
     return render(request, 'events/event_details.html', {
         'event': get_object_or_404(Event, pk=id)})
+
 
 def create_event(request):
     submitted = False;
@@ -34,7 +37,7 @@ def create_event(request):
         if form.is_valid():
             print(2)
             form.save()
-            #event_img = EventImage(image=request.POST['image'], event=event)
+            # event_img = EventImage(image=request.POST['image'], event=event)
             # event_img.save()
             return HttpResponseRedirect('/create_event?submitted=True')
     else:
@@ -42,35 +45,54 @@ def create_event(request):
         if 'submitted' in request.GET:
             submitted = True
         # TODO: Instance new EventCreateForm()
-    return render(request, 'events/create_event.html',{
-        'form': form })
+    return render(request, 'events/create_event.html', {
+        'form': form})
+
 
 def delete_event(request, id):
-    event = get_object_or_404(Event, pk= id)
+    event = get_object_or_404(Event, pk=id)
     event.delete()
     return redirect('event-index')
 
-def update_event(request,id):
-    instance = get_object_or_404(Event,pk=id)
+
+def update_event(request, id):
+    instance = get_object_or_404(Event, pk=id)
     if request == 'POST':
         form = EventUpdateForm(data=request.POST, instance=instance)
         if form.is_valid():
             form.save()
-            return redirect(request,'event-details', id=id)
+            return redirect(request, 'event-details', id=id)
     else:
         form = EventUpdateForm(instance=instance)
         print(2)
     return render(request, 'events/update_event.html', {
         'form': form,
-        'id': id })
+        'id': id})
+
 
 def checkbox_filter(request):
-
-
-
     return render(request, 'events/checkbox.html')
 
-#@login_required
-#def user_profile(request):
+
+def BookEvent(request):
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            ev_id = int(request.POST.get('event_id'))
+            event_check = Event.objects.get(id=ev_id)
+            if(event_check):
+                if(Cart.objects.filter(user=request.user.id, event_id=ev_id)):
+                    return JsonResponse({'status: Event already selected for booking.'})
+                else:
+                    ticket_quantity =  int(request.POST.get('ticket_qty'))
+            else:
+                return JsonResponse({'status':"Event not found."})
+
+        else:
+            return JsonResponse({'status':"Log in to continue!"})
+
+    return redirect('/events')
+
+# @login_required
+# def user_profile(request):
 #    return render(request, 'user_profile.html', {'user': request.user})
-#redirectar á /accounts/profile
+# redirectar á /accounts/profile
