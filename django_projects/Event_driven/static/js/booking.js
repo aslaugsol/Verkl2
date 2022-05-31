b16751996a7d087bc80f70f969858b17e82390f9
aslaugsol <70697649+aslaugsol@users.noreply.github.com>
2022-05-31 19:59:13 +0000

    booking - byrja upp a nytt

--- django_projects/Event_driven/static/js/booking.js
+++ django_projects/Event_driven/static/js/booking.js
@@ -1,23 +1,25 @@
-$(document).ready(function() {
-    $('.BookEventBtn').click(function (e) {
-            e.preventDefault();
-            const event_id = $('.event_id').val();
-            const ticket_qty = $('.qty-input').val();
-            var token = $('input[name=csrfmiddlewaretoken]').val();
-            $.ajax({
-                    method: "POST",
-                    url: "/book-tickets/" + event_id ,
-                    data: {
-                        'event_id': event_id,
-                        'ticket_qty': ticket_qty,
-                        csrfmiddlewaretoken: token
-                    },
-                    success: function (response) {
-                        console.log(response)
-                        alert.success(response.status)
-                    }
-                }
-            )
-        }
-    )
-});
+//$(document).ready(function() {
+    //$('.BookEventBtn').click(function (e) {
+            //e.preventDefault();
+            //const event_id = $('.event_id').val();
+            //const ticket_qty = $('.qty-input').val();
+            //var token = $('input[name=csrfmiddlewaretoken]').val();
+            //$.ajax({
+            //        method: "POST",
+            //        url: "/book-tickets/" + event_id ,
+              //      data: {
+                //        'event_id': event_id,
+                  //      'ticket_qty': ticket_qty,
+                    //    csrfmiddlewaretoken: token
+                    //},
+                    //success: function (response) {
+                      //  console.log(response)
+                       // alert.success(response.status)
+                   // }
+                //}
+           // )
+        //}
+    //)
+//});
+
+console.log('Booking')
