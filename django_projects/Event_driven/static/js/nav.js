c327987df7b0bff428d0365db4b3b56564288da9
solvik00 <70705598+solvik00@users.noreply.github.com>
2022-05-31 22:57:06 +0000

    :)

---
+++ django_projects/Event_driven/static/js/nav.js
@@ -0,0 +1,20 @@
+
+document.getElementById("homeButton").addEventListener("click", (e) => {
+    // go to home page
+});
+
+document.getElementById("logInButton").addEventListener("click", (e) => {
+    // Hide all buttons except home
+});
+
+document.getElementById("registerButton").addEventListener("click", (e) => {
+    // Hide all buttons except home
+});
+
+document.getElementById("logOutButton").addEventListener("click", (e) => {
+    //
+});
+
+document.getElementById("profileButton").addEventListener("click", (e) => {
+    //
+});
